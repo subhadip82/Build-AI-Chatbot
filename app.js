eee011afb68c94edd22609d48d18aeeafb6a334c
Subhadip Bera <subhodipbera10@gmail.com>
2025-07-04 18:43:02 +0530

    project is complet and another problem is solved

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+var typed = new Typed('.neon-heading', {
+  strings: ['Welcome Dip Chat Bot'],
+  typeSpeed: 50,
+  backSpeed: 100,  // ✅ Correct spelling
+  backDelay: 1000,
+  loop: true
+});
+
+
+
 document.getElementById('startBtn').addEventListener('click', () => {
   window.location.href = 'main.html'; // Replace with your actual chatbot page
 });
+
+
