3778054e1331c4bf7ecc27809cbe12876351152c
Subhadip Bera <subhodipbera10@gmail.com>
2025-07-04 18:59:58 +0530

    add text transection

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var typed = new Typed('.neon-heading', {
   strings: ['Welcome Dip Chat Bot'],
   typeSpeed: 50,
-  backSpeed: 100,  // ✅ Correct spelling
+  backSpeed: 100,
   backDelay: 1000,
   loop: true
 });
-
-
 
 document.getElementById('startBtn').addEventListener('click', () => {
   window.location.href = 'main.html'; // Replace with your actual chatbot page
